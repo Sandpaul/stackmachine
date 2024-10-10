f203db359e738ee031781e6614532b8beff73140
Paul Sandford <paul.sandford@bjss.com>
2024-10-10 10:43:03 +0100

    add stack slice to StackMachine() and return error if length of stack is 0.

--- stackmachine.go
+++ stackmachine.go
@@ -2,9 +2,30 @@ package main
 
 import "errors"
 
+
 func StackMachine(commands string)(int, error) {
-	return 0, errors.New("")
+	stack := []int{}
+
+	if len(stack) == 0 {
+		return 0, errors.New("")
+	}
+	return 1, nil
 }
+
+
+// Commands:
+// - an integer (0 to 50000) - the machine pushes this integer onto the stack
+// - POP - removes most recently pushed integer from the stack
+// - DUP - duplicate the last number pushed onto the stack (or keep the stack empty)
+// - `+` - pop the the most recent two numbers, add them together and push the result. If an overflow occurs return an error
+// - `-` - pop the most recent two numbers, subtract the second from the most recent, push the result. Return error if result is below 0
+// - `*` - pop the top two elements from the stack, multiply them together, push the result onto the stack
+// - CLEAR - empties the stack
+// - SUM - pops all elements off the stack, adds them together, pushes result onto the stack. SUM on an empty stack returns an error.
+// - Any other input is invalid - machine must stop and return an error
+
+// After processing all the operators without errors, the machine returns the topmost value from the stack. 
+
 
 func main() {
 	// main is unused - run using 
